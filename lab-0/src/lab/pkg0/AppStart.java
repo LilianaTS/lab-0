4d870efaa0d37f362459d5a995ee8d78f3ea54bc
LillyTS <75341426+LillyTS@users.noreply.github.com>
2021-03-04 09:30:04 +0000

    init AppStart  - added menu for new drink  - added output

---
+++ lab-0/src/lab/pkg0/AppStart.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg0;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lili
+ */
+public class AppStart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int orderNumber = 0;
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Escolha o tamanho da sua bebida");
+        System.out.println ("S - Pequeno");
+        System.out.println ("M - Médio");
+        System.out.println ("L - Grande");
+        char tamanho = scan.next().charAt(0);
+        
+        switch (tamanho) {
+            case 'S':
+                orderNumber+=1;
+                System.out.println("Name         : " + Size.SMALL);
+                System.out.println("toString()   : " + Size.SMALL.toString());
+                System.out.println("Ordem        : " + orderNumber);
+                System.out.println("Valor minimo : " + Size.SMALL.getMinValue());
+                System.out.println("Valor máximo : " + Size.SMALL.getMinValue());
+                System.out.println("Código       : " + Size.SMALL.getCode());
+                break;
+            case 'M':
+                orderNumber+=1;
+                System.out.println("Name         : " + Size.MEDIUM);
+                System.out.println("toString()   : " + Size.MEDIUM.toString());
+                System.out.println("Ordem        : " + orderNumber);
+                System.out.println("Valor minimo : " + Size.MEDIUM.getMinValue());
+                System.out.println("Valor máximo : " + Size.MEDIUM.getMinValue());
+                System.out.println("Código       : " + Size.MEDIUM.getCode());
+                break;
+            case 'L':
+                orderNumber+=1;
+                System.out.println("Name         : " + Size.LARGE);
+                System.out.println("toString()   : " + Size.LARGE.toString());
+                System.out.println("Ordem        : " + orderNumber);
+                System.out.println("Valor minimo : " + Size.LARGE.getMinValue());
+                System.out.println("Valor máximo : " + Size.LARGE.getMinValue());
+                System.out.println("Código       : " + Size.LARGE.getCode());
+                break;
+            default:
+                break;
+        }
+        
+        
+    }
+    
+}
