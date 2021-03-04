2ddc9b1cc3af8219fc07ad5d41fa79048ff81f3a
LillyTS <75341426+LillyTS@users.noreply.github.com>
2021-03-04 10:23:31 +0000

    Improvements to appstart

--- lab-0/src/lab/pkg0/AppStart.java
+++ lab-0/src/lab/pkg0/AppStart.java
@@ -18,45 +18,51 @@ public class AppStart {
      */
     public static void main(String[] args) {
         int orderNumber = 0;
-        Scanner scan = new Scanner (System.in);
-        System.out.println("Escolha o tamanho da sua bebida");
-        System.out.println ("S - Pequeno");
-        System.out.println ("M - Médio");
-        System.out.println ("L - Grande");
-        char tamanho = scan.next().charAt(0);
-        
-        switch (tamanho) {
-            case 'S':
-                orderNumber+=1;
-                System.out.println("Name         : " + Size.SMALL);
-                System.out.println("toString()   : " + Size.SMALL.toString());
-                System.out.println("Ordem        : " + orderNumber);
-                System.out.println("Valor minimo : " + Size.SMALL.getMinValue());
-                System.out.println("Valor máximo : " + Size.SMALL.getMinValue());
-                System.out.println("Código       : " + Size.SMALL.getCode());
-                break;
-            case 'M':
-                orderNumber+=1;
-                System.out.println("Name         : " + Size.MEDIUM);
-                System.out.println("toString()   : " + Size.MEDIUM.toString());
-                System.out.println("Ordem        : " + orderNumber);
-                System.out.println("Valor minimo : " + Size.MEDIUM.getMinValue());
-                System.out.println("Valor máximo : " + Size.MEDIUM.getMinValue());
-                System.out.println("Código       : " + Size.MEDIUM.getCode());
-                break;
-            case 'L':
-                orderNumber+=1;
-                System.out.println("Name         : " + Size.LARGE);
-                System.out.println("toString()   : " + Size.LARGE.toString());
-                System.out.println("Ordem        : " + orderNumber);
-                System.out.println("Valor minimo : " + Size.LARGE.getMinValue());
-                System.out.println("Valor máximo : " + Size.LARGE.getMinValue());
-                System.out.println("Código       : " + Size.LARGE.getCode());
-                break;
-            default:
-                break;
+        while (true) {
+            
+            Scanner scan = new Scanner (System.in);
+            System.out.println("Escolha o tamanho da sua bebida");
+            System.out.println ("S - Pequeno");
+            System.out.println ("M - Médio");
+            System.out.println ("L - Grande");
+            System.out.println("E - exit");
+            System.out.print("Tamanho: ");
+            String tamanho = scan.next().toUpperCase();
+            
+            switch (tamanho) {
+                case "S":
+                    orderNumber+=1;
+                    System.out.println("Name         : " + Size.SMALL);
+                    System.out.println("toString()   : " + Size.SMALL.toString());
+                    System.out.println("Ordem        : " + orderNumber);
+                    System.out.println("Valor minimo : " + Size.SMALL.getMinValue());
+                    System.out.println("Valor máximo : " + Size.SMALL.getMinValue());
+                    System.out.println("Código       : " + Size.SMALL.getCode());
+                    break;
+                case "M":
+                    orderNumber+=1;
+                    System.out.println("Name         : " + Size.MEDIUM);
+                    System.out.println("toString()   : " + Size.MEDIUM.toString());
+                    System.out.println("Ordem        : " + orderNumber);
+                    System.out.println("Valor minimo : " + Size.MEDIUM.getMinValue());
+                    System.out.println("Valor máximo : " + Size.MEDIUM.getMinValue());
+                    System.out.println("Código       : " + Size.MEDIUM.getCode());
+                    break;
+                case "L":
+                    orderNumber+=1;
+                    System.out.println("Name         : " + Size.LARGE);
+                    System.out.println("toString()   : " + Size.LARGE.toString());
+                    System.out.println("Ordem        : " + orderNumber);
+                    System.out.println("Valor minimo : " + Size.LARGE.getMinValue());
+                    System.out.println("Valor máximo : " + Size.LARGE.getMinValue());
+                    System.out.println("Código       : " + Size.LARGE.getCode());
+                    break;
+                case "E":
+                    return;
+                default:
+                    break;
+            }
         }
-        
         
     }
     
