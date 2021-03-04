6941328b517efe564f375182c249a51af7c3c833
LillyTS <75341426+LillyTS@users.noreply.github.com>
2021-03-04 08:56:56 +0000

    Definição do tamanho das chávenas

---
+++ lab-0/src/lab/pkg0/Size.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg0;
+
+/**
+ *
+ * @author Lili
+ */
+public enum Size {
+
+ SMALL(32, 36, "Pequeno",'S'),
+ MEDIUM(37, 44, "Médio",'M'),
+ LARGE(45, 52, "Grande",'L');
+ private final String description;
+ private final int minValue;
+ private final int maxValue;
+ private final char code;
+
+ private Size(int minValue, int maxValue, String description, char code){
+ this.description=description;
+ this.minValue=minValue;
+ this.maxValue=maxValue;
+ this.code=code;
+ }
+ @Override
+ public String toString() {
+ return this.description;
+ }
+ public String getDescription() {
+ return description;
+ }
+ public int getMinValue() {
+ return minValue;
+ }
+ public int getMaxValue() {
+ return maxValue;
+ }
+ public char getCode() {
+ return code;
+ }
+
+}
